a87c67c82ad594f8f8dde0e49fedabcef07c4c4f
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-09-14 14:23:39 -0700

    Amp

--- subdirectories/scripts/build_cookbook.py
+++ subdirectories/scripts/build_cookbook.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 
 _REPO_ROOT = "https://github.com/langchain-ai/langsmith-cookbook"
 
+
 def convert_notebooks_to_markdown(root_path: str) -> None:
     """
     Convert all Jupyter notebooks in the directory to Markdown and save images.
@@ -43,15 +44,15 @@ def convert_notebooks_to_markdown(root_path: str) -> None:
 
                     output_post_save(markdown, resources)
                     markdown = clean_markdown(markdown)
-
                     md_file_path = os.path.join(dirpath, file.replace(".ipynb", ".md"))
                     with open(md_file_path, "w", encoding="utf-8") as md_file:
                         md_file.write(markdown)
 
+
 def flexible_table_replacement(markdown: str, table_str: str) -> str:
     """
     Replace the table in the markdown with the given table string using a more flexible matching.
-    
+
     Args:
     - markdown (str): The original markdown content.
     - table_str (str): The exact table string to replace with.
@@ -61,15 +62,20 @@ def flexible_table_replacement(markdown: str, table_str: str) -> str:
     """
     start_of_table = "<table"
     end_of_table = "</table>"
-    
+
     start_index = markdown.find(start_of_table)
-    end_index = markdown.find(end_of_table, start_index) + len(end_of_table) if start_index != -1 else -1
-    
+    end_index = (
+        markdown.find(end_of_table, start_index) + len(end_of_table)
+        if start_index != -1
+        else -1
+    )
+
     # If both start and end are found, replace
     if start_index != -1 and end_index != -1:
         return markdown[:start_index] + table_str + markdown[end_index:]
     else:
         return markdown
+
 
 def clean_markdown(markdown: str) -> str:
     soup = BeautifulSoup(markdown, "html.parser")
@@ -80,10 +86,11 @@ def clean_markdown(markdown: str) -> str:
     markdown = remove_stray_divs(markdown)
     return markdown
 
+
 def remove_stray_divs(markdown: str) -> str:
     """
     Remove stray and empty <div> tags from the markdown content.
-    
+
     Args:
     - markdown (str): The original markdown content.
 
@@ -91,23 +98,24 @@ def remove_stray_divs(markdown: str) -> str:
     - str: The markdown without stray and empty <div> tags.
     """
     soup = BeautifulSoup(markdown, "html.parser")
-    
+
     # Remove empty <div> tags
     for div in soup.find_all("div"):
-        if not div.contents or all(isinstance(c, str) and not c.strip() for c in div.contents):
+        if not div.contents or all(
+            isinstance(c, str) and not c.strip() for c in div.contents
+        ):
             div.extract()
 
-    # Convert back to string and check for stray <div> tags
-    cleaned_content = str(soup)
+    cleaned_content = str(soup).replace("&amp;", "&")
     cleaned_content = cleaned_content.replace("<div>", "").replace("</div>", "")
-    
+
     return cleaned_content
 
 
 def remove_dataframe_styles(markdown: str) -> str:
     """
     Remove style blocks related to Pandas DataFrames from the markdown content.
-    
+
     Args:
     - markdown (str): The original markdown content.
 
@@ -115,12 +123,13 @@ def remove_dataframe_styles(markdown: str) -> str:
     - str: The markdown without the DataFrame style blocks.
     """
     soup = BeautifulSoup(markdown, "html.parser")
-    
+
     # Find all <style> tags with the 'scoped' attribute (commonly used by Pandas DataFrame styles)
     for style_tag in soup.find_all("style", attrs={"scoped": True}):
         style_tag.extract()  # Remove the tag from the soup object
 
     return str(soup)
+
 
 def html_table_to_markdown(html_content: str) -> str:
     """
@@ -272,7 +281,6 @@ We suggest running the code by forking or cloning the repository.
                     content = content.replace("<", "&lt;")
                     content = content.replace(">", "&gt;")
                     content = content.replace("/README.md)", "/)")
-
                     with open(dest, "w", encoding="utf-8") as md_file:
                         md_file.write(content)
 
