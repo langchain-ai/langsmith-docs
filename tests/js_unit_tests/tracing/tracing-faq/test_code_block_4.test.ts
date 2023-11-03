3fc4b54d19e3aa79d61719b33ad44a103f54047f
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-11-03 10:08:57 -0700

    version

--- tests/js_unit_tests/tracing/tracing-faq/test_code_block_4.test.ts
+++ tests/js_unit_tests/tracing/tracing-faq/test_code_block_4.test.ts
@@ -1,5 +1,15 @@
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import {  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+} from "langchain/prompts";
+
 
 test('test_code_block_4', async () => {
+    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+      HumanMessagePromptTemplate.fromTemplate("{query}"),
+    ]);
+    
+    const chain = chatPrompt.pipe(new ChatOpenAI());
     const configuredChain = chain.withConfig({ runName: "MyCustomChain" });
     await configuredChain.invoke({query: "What is the meaning of life?"});
 });
