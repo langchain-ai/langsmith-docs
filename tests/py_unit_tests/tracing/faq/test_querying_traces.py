872c550c325847334f7983690b0c4211e4e054cd
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-02-14 14:33:53 -0800

    update test

--- tests/py_unit_tests/tracing/faq/test_querying_traces.py
+++ tests/py_unit_tests/tracing/faq/test_querying_traces.py
@@ -15,24 +15,18 @@ async def test_code_block_1():
 
 @pytest.mark.asyncio
 async def test_code_block_2():
-    import json
-    to_search = {'user_id': '4070f233-f61e-44eb-bff1-da3c163895a3'}
-    list(client.list_runs(project_name='default', filter=f"has(metadata, '{json.dumps(to_search)}')"))
+    to_search = {'user_id': ''}
+    list(client.list_runs(project_name='default', filter="eq(metadata_key, 'user_id')"))
+    list(client.list_runs(project_name='default', filter="and(eq(metadata_key, 'user_id'), eq(metadata_value, '4070f233-f61e-44eb-bff1-da3c163895a3'))"))
 
 @pytest.mark.asyncio
 async def test_code_block_3():
-    import json
-    to_search = {'environment': 'production'}
-    list(client.list_runs(project_name='default', filter=f"has(metadata, '{json.dumps(to_search)}')"))
+    list(client.list_runs(project_name='default', filter="and(eq(metadata_key, 'environment'), eq(metadata_value, 'production'))"))
 
 @pytest.mark.asyncio
 async def test_code_block_4():
-    import json
-    to_search = {'conversation_id': 'a1b2c3d4-e5f6-7890'}
-    list(client.list_runs(project_name='default', filter=f"has(metadata, '{json.dumps(to_search)}')"))
+    list(client.list_runs(project_name='default', filter="and(eq(metadata_key, 'conversation_id'), eq(metadata_value, 'a1b2c3d4-e5f6-7890'))"))
 
 @pytest.mark.asyncio
 async def test_code_block_5():
-    import json
-    to_search = {'conversation_id': '69b12c91-b1e2-46ce-91de-794c077e8151'}
-    list(client.list_runs(project_name='default', filter=f"and(has(metadata, '{json.dumps(to_search)}'), eq(name, 'ChatOpenAI'))"))
+    list(client.list_runs(project_name='default', filter="and(eq(name, 'ChatOpenAI'), eq(metadata_key, 'conversation_id'), eq(metadata_value, '69b12c91-b1e2-46ce-91de-794c077e8151'))"))
