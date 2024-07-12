48f768a3a9e2a64ee8ec51e01021c4335cc83f52
Brian Vander Schaaf <brian@langchain.dev>
2024-07-12 17:55:58 -0400

    simplify

--- src/components/RegionalUrls.js
+++ src/components/RegionalUrls.js
@@ -1,19 +1,11 @@
 import React, { useState, useEffect } from "react";
-import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
 export const RegionalUrl = () => {
-  var url;
-  var setUrl;
-  if (ExecutionEnvironment.canUseDOM) {
-    [url, setUrl] = useState(
-      localStorage.getItem("ls:docs:langsmithUrl") ||
-        "https://smith.langchain.com"
-    );
-  } else {
-    [url, setUrl] = useState("https://smith.langchain.com");
-  }
+  const [url, setUrl] = useState("https://smith.langchain.com");
 
   useEffect(() => {
+    const storedUrl = localStorage.getItem('ls:docs:langsmithUrl') || 'https://smith.langchain.com';
+    setUrl(storedUrl);
     const handleStorageChange = () => {
       setUrl(
         localStorage.getItem("ls:docs:langsmithUrl") ||
