09bc48ad76eedcafe8fae59c03808e91e751d55f
jacoblee93 <jacoblee93@gmail.com>
2024-07-23 13:26:01 -0700

    Polish

--- src/components/LangGraphCodeBlocks.js
+++ src/components/LangGraphCodeBlocks.js
@@ -1,3 +1,9 @@
+import CodeBlock from "@theme/CodeBlock";
+import React from "react";
+import TabItem from "@theme/TabItem";
+import Tabs from "@theme/Tabs";
+import { CodeTabs } from "./InstructionsWithCode";
+
 export function LangGraphInstallationCodeTabs() {
   return (
     <CodeTabs
@@ -83,6 +89,7 @@ function shouldContinue(state: AgentState) {
 
 async function callModel(state: AgentState) {
   const messages = state.messages;
+  // Invoking \`model\` will automatically infer the correct tracing context
   const response = await model.invoke(messages);
   return { messages: [response] };
 }
@@ -138,13 +145,14 @@ def should_continue(state: MessagesState) -> Literal["tools", "__end__"]:
 
 def call_model(state: MessagesState):
     messages = state['messages']
+
+    # Invoking \`model\` will automatically infer the correct tracing context
     response = model.invoke(messages)
     return {"messages": [response]}
 
 
 workflow = StateGraph(MessagesState)
 
-# Define the two nodes we will cycle between
 workflow.add_node("agent", call_model)
 workflow.add_node("tools", tool_node)
 
@@ -208,6 +216,106 @@ export function LangGraphWithoutLangChainInstallationCodeTabs() {
 }
 
 export function LangGraphWithoutLangChainCodeTabs({}) {
+  const pythonBlock = `import json
+import openai
+import operator
+
+from langsmith import traceable
+from langsmith.wrappers import wrap_openai
+
+from typing import Annotated, Literal, TypedDict
+
+from langgraph.graph import StateGraph
+
+class State(TypedDict):
+    messages: Annotated[list, operator.add]
+
+tool_schema = {
+    "type": "function",
+    "function": {
+        "name": "search",
+        "description": "Call to surf the web.",
+        "parameters": {
+            "type": "object",
+            "properties": {"query": {"type": "string"}},
+            "required": ["query"],
+        },
+    },
+}
+
+# Decorating the tool function will automatically trace it with the correct context
+@traceable(run_type="tool", name="Search Tool")
+def search(query: str):
+    """Call to surf the web."""
+    if "sf" in query.lower() or "san francisco" in query.lower():
+        return "It's 60 degrees and foggy."
+    return "It's 90 degrees and sunny."
+
+tools = [search]
+
+def call_tools(state):
+    function_name_to_function = {"search": search}
+    messages = state["messages"]
+
+    tool_call = messages[-1]["tool_calls"][0]
+    function_name = tool_call["function"]["name"]
+    function_arguments = tool_call["function"]["arguments"]
+    arguments = json.loads(function_arguments)
+
+    function_response = function_name_to_function[function_name](**arguments)
+    tool_message = {
+        "tool_call_id": tool_call["id"],
+        "role": "tool",
+        "name": function_name,
+        "content": function_response,
+    }
+    return {"messages": [tool_message]}
+
+wrapped_client = wrap_openai(openai.Client())
+
+def should_continue(state: State) -> Literal["tools", "__end__"]:
+    messages = state["messages"]
+    last_message = messages[-1]
+    if last_message["tool_calls"]:
+        return "tools"
+    return "__end__"
+
+
+def call_model(state: State):
+    messages = state["messages"]
+    # Calling the wrapped client will automatically infer the correct tracing context
+    response = wrapped_client.chat.completions.create(
+        messages=messages, model="gpt-3.5-turbo", tools=[tool_schema]
+    )
+    raw_tool_calls = response.choices[0].message.tool_calls
+    tool_calls = [tool_call.to_dict() for tool_call in raw_tool_calls] if raw_tool_calls else []
+    response_message = {
+        "role": "assistant",
+        "content": response.choices[0].message.content,
+        "tool_calls": tool_calls,
+    }
+    return {"messages": [response_message]}
+
+
+workflow = StateGraph(State)
+
+workflow.add_node("agent", call_model)
+workflow.add_node("tools", call_tools)
+
+workflow.add_edge("__start__", "agent")
+workflow.add_conditional_edges(
+    "agent",
+    should_continue,
+)
+workflow.add_edge("tools", 'agent')
+
+app = workflow.compile()
+
+final_state = app.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
+final_state["messages"][-1]["content"]`;
+
   const tsBlock = `import OpenAI from "openai";
 import { StateGraph } from "@langchain/langgraph";
 import { wrapOpenAI } from "langsmith/wrappers/openai";
@@ -236,6 +344,7 @@ const toolSchema: OpenAI.ChatCompletionTool = {
   }
 };
 
+// Wrapping the tool function will automatically trace it with the correct context
 const search = traceable(async ({ query }: { query: string }) => {
   if (
     query.toLowerCase().includes("sf") ||
@@ -268,6 +377,7 @@ const callTools = async ({ messages }: GraphState) => {
 }
 
 const callModel = async ({ messages }: GraphState) => {
+  // Calling the wrapped client will automatically infer the correct tracing context
   const response = await wrappedClient.chat.completions.create({
     messages,
     model: "gpt-4o-mini",
@@ -317,114 +427,22 @@ await graph.invoke({
 });`;
 
   return (
-    <Tabs groupId="client-language" className="code-tabs">
-      <TabItem key="python" value="python" label="Python">
-        <CodeBlock className="python" language="python">
-          {`import json
-import openai
-import operator
-
-from langsmith import traceable
-from langsmith.wrappers import wrap_openai
-
-from typing import Annotated, Literal, TypedDict
-
-from langgraph.graph import StateGraph
-
-class State(TypedDict):
-    messages: Annotated[list, operator.add]
-
-tool_schema = {
-    "type": "function",
-    "function": {
-        "name": "search",
-        "description": "Call to surf the web.",
-        "parameters": {
-            "type": "object",
-            "properties": {"query": {"type": "string"}},
-            "required": ["query"],
+    <CodeTabs
+      groupId="client-language"
+      tabs={[
+        {
+          value: "python",
+          label: "Python",
+          content: pythonBlock,
         },
-    },
-}
-
-@traceable(run_type="tool", name="Search Tool")
-def search(query: str):
-    """Call to surf the web."""
-    if "sf" in query.lower() or "san francisco" in query.lower():
-        return "It's 60 degrees and foggy."
-    return "It's 90 degrees and sunny."
-
-tools = [search]
-
-def call_tools(state):
-    function_name_to_function = {"search": search}
-    messages = state["messages"]
-
-    tool_call = messages[-1]["tool_calls"][0]
-    function_name = tool_call["function"]["name"]
-    function_arguments = tool_call["function"]["arguments"]
-    arguments = json.loads(function_arguments)
-
-    function_response = function_name_to_function[function_name](**arguments)
-    tool_message = {
-        "tool_call_id": tool_call["id"],
-        "role": "tool",
-        "name": function_name,
-        "content": function_response,
-    }
-    return {"messages": [tool_message]}
-
-wrapped_client = wrap_openai(openai.Client())
-
-def should_continue(state: State) -> Literal["tools", "__end__"]:
-    messages = state["messages"]
-    last_message = messages[-1]
-    if last_message["tool_calls"]:
-        return "tools"
-    return "__end__"
-
-
-def call_model(state: State):
-    messages = state["messages"]
-    response = wrapped_client.chat.completions.create(
-        messages=messages, model="gpt-3.5-turbo", tools=[tool_schema]
-    )
-    raw_tool_calls = response.choices[0].message.tool_calls
-    tool_calls = [tool_call.to_dict() for tool_call in raw_tool_calls] if raw_tool_calls else []
-    response_message = {
-        "role": "assistant",
-        "content": response.choices[0].message.content,
-        "tool_calls": tool_calls,
-    }
-    return {"messages": [response_message]}
-
-
-workflow = StateGraph(State)
-
-# Define the two nodes we will cycle between
-workflow.add_node("agent", call_model)
-workflow.add_node("tools", call_tools)
-
-workflow.add_edge("__start__", "agent")
-workflow.add_conditional_edges(
-    "agent",
-    should_continue,
-)
-workflow.add_edge("tools", 'agent')
-
-app = workflow.compile()
-
-final_state = app.invoke(
-    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
-)
-final_state["messages"][-1]["content"]`}
-        </CodeBlock>
-      </TabItem>
-      <TabItem key="typescript" value="typescript" label="TypeScript">
-        <CodeBlock className="typescript" language="typescript">
-          {tsBlock}
-        </CodeBlock>
-      </TabItem>
-    </Tabs>
+        {
+          value: "typescript",
+          label: "TypeScript",
+          content: tsBlock,
+          caption:
+            "**Note:** The below example requires `langsmith>=0.1.39` and `@langchain/langgraph>=0.0.31`",
+        },
+      ]}
+    ></CodeTabs>
   );
 }
