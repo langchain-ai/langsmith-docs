61417674bc165fc80fc3e3b2403b44f07c9ff45a
Jake Rachleff <jake@langchain.dev>
2024-10-22 16:49:20 -0700

    evaluation working

---
+++ docs/evaluation/index.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import { useHistory } from '@docusaurus/router';
+
+export default function Home() {
+  const history = useHistory();
+
+  useEffect(() => {
+    // Redirect to the subdirectory '/docs'
+    history.push('/docs');
+  }, [history]);
+
+  return null; // Return nothing because we're redirecting
+}
