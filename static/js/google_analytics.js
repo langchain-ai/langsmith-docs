5193d6cdf7bfeab65aa33f92513b62c7295f8437
Danny Lambert <daniellambert@Dannys-MacBook-Pro.local>
2025-05-19 11:22:18 -0500

    removed google analytics static file

--- static/js/google_analytics.js
+++
@@ -1,8 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-
-gtag("config", "G-62DTR64PQZ");
-gtag("config", "G-47WX3HKKY2");
